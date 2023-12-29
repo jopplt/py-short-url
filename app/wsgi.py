7db8bd2578b71bfa9afb3cf99824a365c3a0ed46
Josep Palet <josep@sento.io>
2023-12-29 13:21:23 +0100

    switch to gunicorn

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,15 +1,4 @@
-import uvicorn
 from bootstrap import app
 from entrypoints.fastapi import ApiFactory
 
-api_factory = ApiFactory()
-api = api_factory.create(application=app)
-
-if __name__ == "__main__":
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument("-p", "--port", default=5000, type=int)
-    args = parser.parse_args()
-    port = args.port
-    uvicorn.run(api, host="0.0.0.0", port=port)
+api = ApiFactory().create(application=app)
